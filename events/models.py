c71e2db83b73f5cba09ede44d154755a3b4417cc
Andrey Kropotin <tork713@gmail.com>
2021-05-15 08:05:38 +0700

    continue 15.05.21

--- events/models.py
+++ events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	image = models.ImageField(upload_to='event_images/')
+	text = models.TextField()
+	
+
--- project05/settings.py
+++ project05/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    'blog.apps.BlogConfig',     
+    'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/my_ media/'
